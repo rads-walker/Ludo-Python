6502efa067853fffd3a90240957abcb3d0cb8caa
Danilo Rafael Rocha SiIva <danilor4591@yahoo.com>
2019-03-21 01:24:51 -0300

    Create Att.py
    arquivo com as proximas atualizações

---
+++ Att.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 21 00:38:43 2019
+
+@author: danil
+"""
+
++ Adicionar um timer para dar delay na animação das peças
++ Implementar a função liberar peça
++ Implementar a função mover peça
